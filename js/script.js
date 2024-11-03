1aa9ab6f2ff27755d47ae312dd4940369de848b6
Sanuth Bibin.B <165442353+Sanuthb@users.noreply.github.com>
2024-11-03 19:57:57 +0530

    js

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// variable decleration
+//a=112;// a int 
+
+//var,let,const
+
+// var a =12;
+// let a=12;
+//const a=12;
+
+//console.log(a); print statement
+
+// operators
+// Assignment Operator  
+// logical operator
+
+
+// Ternary Operators
+
+//if-else statement // stynax ?:
+
+// condition ? true : false 
+
+// datatypes
+// 1. integer
+// 2. boolean
+// 3. string
+// 4.float
+// 5.objects [],{},date
+
+
+// function declaration
+
+// function func_name(arguments,agr1){
+//     console.log('hello'); //statement
+//     return;
+// }
+
+// function call
+
+// func_name('world');
+
+//arrow function
+
+// var = () =>{}
+
+const func_dec= ()=>{
+    console.log('hello');
+    return;
+
+}
+
+// func_dec()
+
+// find sum of two numbers using arrow function 
+
+// var a=12;
+// var b=34;
+
+// const sum = (a,b) => a+b;
+
+// console.log(sum(a,b));
+
+
+
+// objects how to create objects??
+
+const car = {
+    brand: 'Tesla',
+    model: 'Model S',
+    year: 2021,
+    color: 'Black',
+    startEngine: () => {
+        console.log('Engine started');
+    }
+}
+
+// console.log(car)
+
+console.log(car.brand)
+
+console
